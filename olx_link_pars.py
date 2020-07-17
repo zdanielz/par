44932b4e52f0258a3e8d5b8a0c4eb3f9a6c71303
zdanielz <43722126+zdanielz@users.noreply.github.com>
2020-07-17 17:18:03 +0300

    Add files via upload

--- olx_link_pars.py
+++ olx_link_pars.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import re
 import requests
 import json
 
@@ -16,15 +17,35 @@ for i in range(len(links)):
     links[i] = links[i][0:-1]
     i += 1
 
-while link_number < 10:
+
+while link_number < 2:
 
     offer_details_name = []
     offer_details_value = []
 
-    html_l = requests.get(links[link_number], headers=headers).content.decode("utf-8")
+    requests_dat = requests.get(links[link_number], headers=headers)
+
+    html_l = requests_dat.content.decode("utf-8")
+    cookies = requests_dat.cookies
+
     offer_details_name_raw = BeautifulSoup(html_l, 'lxml').find("ul", {"class": "offer-details"}).find_all("span", {"class": "offer-details__name"})
     offer_details_value_raw = BeautifulSoup(html_l, 'lxml').find("ul", {"class": "offer-details"}).find_all("strong", {"class": "offer-details__value"})
     description = "Описание: " + str(BeautifulSoup(html_l, 'lxml').find("div", {"class": "descriptioncontent"}).find("div", {"id": "textContent"}).text.replace("   ", "").replace("\r", "").replace("\n", ""))
+
+    ob_id_tag = BeautifulSoup(html_l, 'lxml').find("div", {"class": "contact-button"})
+
+    pattern = re.compile('\'id\':(\'.*?\'),')
+    ob_id = re.findall(pattern, str(ob_id_tag))[0][1:-1]
+
+    pattern = re.compile('var phoneToken = (\'.*?\');')
+    phoneToken = re.findall(pattern, str(html_l))[0][1:-1]
+
+    print(ob_id)
+    print(phoneToken)
+    print(cookies)
+
+    phone = requests.get("https://www.olx.ua/ajax/misc/contact/phone/{0}/?pt={1}".format(ob_id, phoneToken), headers=headers, cookies=cookies).content
+    print(phone)
 
     for i in range(len(offer_details_name)):
         data.append("{0}: {1}".format(offer_details_name_raw[i].text, offer_details_value_raw[i].text))
